06cf63ad962ecbccb89790abe073905f09c95aa2
rudokaite <137444283+rudokaite@users.noreply.github.com>
2026-01-07 12:30:37 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,12 @@
 
-  import { defineConfig } from 'vite';
-  import react from '@vitejs/plugin-react-swc';
-  import path from 'path';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+  base: '/Cvrudokaite/', // 👈 repo name EXACTLY
+})
+
 
   export default defineConfig({
     plugins: [react()],
